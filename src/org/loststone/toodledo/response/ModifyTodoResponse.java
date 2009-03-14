c5e927a9fd0fe0b9cd708c8ef7e5ac02a22b3885
Marc <phlegias@gmail.com>
2009-03-14 17:59:38 +0000

    Ok, all coded, now to test.

---
+++ src/org/loststone/toodledo/response/ModifyTodoResponse.java
@@ -0,0 +1,37 @@
+package org.loststone.toodledo.response;
+
+import org.loststone.toodledo.ToodledoApiException;
+
+
+public class ModifyTodoResponse extends Response {
+
+	public ModifyTodoResponse(String resp) {
+		super(resp);
+		if (response.contains("error")) 
+			this.succeed = false;
+		else
+			this.succeed = true;
+	}
+	
+	@Override
+	public int getResponseResult() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	// TODO refactor to the parent class.
+	public String getResponseContent() throws ToodledoApiException {
+		String result = null;		
+		// check for errors:
+		if (response.contains("error")) {
+			this.succeed = false;
+			throw new ToodledoApiException(response.substring(response.indexOf("<error>")+7, 
+					response.indexOf("</error>")));
+		}
+		
+		result = response.substring(response.indexOf("<success>")+9, response.indexOf("</success>"));
+		this.succeed = true;
+		return result; 
+	}
+
+}
