898a1cffa1785268015e154df123d33c8173e6df
lant <lant@Barrett.local>
2010-01-19 21:30:41 +0000

    Added new functionality: remove folder.
    Also, this method and the 'deleteTodo' returns a new delete generic response as it is exactly the same.
    
    No testing yet.

---
+++ src/org/loststone/toodledo/response/GenericDeleteResponse.java
@@ -0,0 +1,24 @@
+package org.loststone.toodledo.response;
+
+import org.loststone.toodledo.exception.ToodledoApiException;
+
+
+public class GenericDeleteResponse extends Response {
+
+	public GenericDeleteResponse(String resp) {
+		super(resp);
+	}
+
+	public String getResponseContent() throws ToodledoApiException {
+		String result = null;		
+		// check for errors:
+		if (!this.succeeded()) {
+			throw new ToodledoApiException(response.substring(response.indexOf("<error>")+7, 
+					response.indexOf("</error>")));
+		}
+		
+		result = response.substring(response.indexOf("<success>")+9, response.indexOf("</success>"));
+		return result; 
+	}
+
+}
