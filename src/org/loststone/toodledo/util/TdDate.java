ded04c5a78d94e75e50608f61e97148ab67538b5
Marc <phlegias@gmail.com>
2010-09-22 23:05:02 +0100

    Applied the patch from John Gentilin fixing some stuff in the Dates.

--- src/org/loststone/toodledo/util/TdDate.java
+++ src/org/loststone/toodledo/util/TdDate.java
@@ -13,14 +13,19 @@ public class TdDate {
 	}
 	
 	public TdDate(String date) {
-		dt = new DateTime(date);
+	  try {
+	    dt = new DateTime(date);
+	  } catch (Exception e) { dt = null; }
 	}
 	
 	public TdDate(int year, int month, int day) {
-		dt = new DateTime(year, month, day, 0,0,0,0);
+	  try {
+	    dt = new DateTime(year, month, day, 0,0,0,0);
+	  } catch (Exception e) { dt = null; }
 	}
 	
 	public String toString() {
+	  if (dt == null) return "";
 		DateTimeFormatter fmt = DateTimeFormat.forPattern("YYYY-MM-dd");
 		return dt.toString(fmt);
 	}
--- src/org/loststone/toodledo/util/TdDateTime.java
+++ src/org/loststone/toodledo/util/TdDateTime.java
@@ -11,14 +11,27 @@ public class TdDateTime extends TdDate {
 	}
 	
 	public TdDateTime(String dateTime) {
-		this.dt = new DateTime(dateTime);
+	  DateTimeFormatter parser =
+	   DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+	  
+	  try { this.dt = parser.parseDateTime(dateTime); }
+	  catch (Exception e) { this.dt = null; }
+	  
+	  try {
+  	  if (this.dt == null) {
+  		 this.dt = new DateTime(dateTime);
+  	  }
+	  } catch(Exception e) { this.dt = null; }
 	}
 	
 	public TdDateTime(int year, int month, int day, int hour, int minute, int second) {
-		this.dt = new DateTime(year, month, day, hour, minute, second, 0);
+	  try {
+	    this.dt = new DateTime(year, month, day, hour, minute, second, 0);
+	  } catch (Exception e) { this.dt = null; }
 	}
 	
 	public String toString() {
+	  if (this.dt == null) return "";
 		DateTimeFormatter fmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss");
 		return dt.toString(fmt);
 	}
